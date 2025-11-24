4f2ff052cff711dfbe2e3ef8c42d989f54b479cf
Ikram33-hub <az.ikramel@gmail.com>
2025-11-25 00:02:58 +0100

    Initial commit - setup React project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontSize: "20px" }}>
+      <h1>GitHub User Search</h1>
+      <p>Welcome Ikram! Your project is running 🎉</p>
+    </div>
+  )
+}
+
+export default App
